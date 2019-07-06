dfdc3d6523a2e6d326c9194c9d9040bf682a5278
Mohammed-Q96 <mhmmade@gmail.com>
2019-07-07 00:41:30 +0300

    last modefication

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-
 (() => {
    let slideIndex = 1;
 
@@ -14,9 +13,7 @@
       if (n > slides.length) {
          slideIndex = 1;
       }
-      if (n < 1) {
-         slideIndex = slides.length; // 3 for now ===> fix it later
-      }
+
       for (i = 0; i < slides.length; i++) {
          slides[i].style.display = 'none';
       }
@@ -38,52 +35,60 @@
    }
 })();
 /*===========> About Us animation <=========== */
-    // get the element to animate
+// get the element to animate
 var element = document.getElementsByClassName('box');
-var elementHeight = element[0].clientHeight-500;
+var elementHeight = element[0].clientHeight - 500;
 
 // listen for scroll event and call animate function
 document.addEventListener('scroll', animate);
 
 // check if element is in view
 function inView() {
-  // get window height
-  var windowHeight = window.innerHeight;
-  // get number of pixels that the document is scrolled
-  var scrollY = window.scrollY || window.pageYOffset;
-  
-  // get current scroll position (distance from the top of the page to the bottom of the current viewport)
-  var scrollPosition = scrollY + windowHeight;
-  // get element position (distance from the top of the page to the bottom of the element)
-  var elementPosition = element[3].getBoundingClientRect().top + scrollY + elementHeight;
+   // get window height
+   var windowHeight = window.innerHeight;
+   // get number of pixels that the document is scrolled
+   var scrollY = window.scrollY || window.pageYOffset;
 
-  // is scroll position greater than element position? (is element in view?)
-  if (scrollPosition > elementPosition) {
-    console.log("dasdasd");
-    return true;
-  }
-  
-  return false;
+   // get current scroll position (distance from the top of the page to the bottom of the current viewport)
+   var scrollPosition = scrollY + windowHeight;
+   // get element position (distance from the top of the page to the bottom of the element)
+   var elementPosition =
+      element[3].getBoundingClientRect().top + scrollY + elementHeight;
+
+   // is scroll position greater than element position? (is element in view?)
+   if (scrollPosition > elementPosition) {
+      console.log('dasdasd');
+      return true;
+   }
+
+   return false;
 }
 
 // animate element when it is in view
 function animate() {
-  // is element in view?
-  if (inView()) {
+   // is element in view?
+   if (inView()) {
       // element is in view, add class to element
-      for(let i = 0 ; i< element.length ; ++i){
-        element[i].classList.add('animated','animatedFadeInUp','fadeInUp','opacity-1');
+      for (let i = 0; i < element.length; ++i) {
+         element[i].classList.add(
+            'animated',
+            'animatedFadeInUp',
+            'fadeInUp',
+            'opacity-1'
+         );
       }
-  }
+   }
 }
 /*===========> End of About Us animation <=========== */
 /*===========> Scroll to Top <=========== */
 var timeOut;
 function scrollToTop() {
-    if (document.body.scrollTop!=0 || document.documentElement.scrollTop!=0){
-        window.scrollBy(0,-50);
-        timeOut=setTimeout('scrollToTop()',10);
-    }
-    else clearTimeout(timeOut);
+   if (
+      document.body.scrollTop != 0 ||
+      document.documentElement.scrollTop != 0
+   ) {
+      window.scrollBy(0, -50);
+      timeOut = setTimeout('scrollToTop()', 10);
+   } else clearTimeout(timeOut);
 }
 /*===========> End of Scroll to Top <=========== */
